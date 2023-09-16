ebae5e534efacd2b656fdcb485d5db657db22a97
Ahmet Karadağ <akkaradaga@gmail.com>
2023-09-16 21:21:35 +0300

    Initial Commit

---
+++ FavoriteList/FavoriteListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteListApp.swift
+//  FavoriteList
+//
+//  Created by ahmet karadağ on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
